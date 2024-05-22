4180bfac659fdeb6c401843de3eba178ef4a2c96
Ryan Romig <rromig92@gmail.com>
2024-05-22 15:18:28 -0700

    init scripts

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-source ${PWD}/scripts/bootstrap.sh
-./scripts/make_img.sh
-./scripts/format_image.sh
-./scripts/populate_boot.sh
-./scripts/finish_build.sh
+. scripts/bootstrap.sh
+. scripts/make_img.sh
+. scripts/format_image.sh
+. scripts/populate_boot.sh
+#populate rootfs
+
+. scripts/finish_build.sh
