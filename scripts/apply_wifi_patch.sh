32574eb7da162b156e8ada38b30387e18c3cdb33
Ryan Romig <rromig92@gmail.com>
2024-05-23 20:53:57 -0700

    update patches

--- scripts/apply_wifi_patch.sh
+++ scripts/apply_wifi_patch.sh
@@ -2,4 +2,8 @@
 
 cp patch/Kconfig ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Kconfig
 cp patch/Makefile ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Makefile
-cp patch/*.dts ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
+
+cp patch/dts/melodia.dts ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
+cp patch/dts/Makefile ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
+
+cp patch/melodia_defconfig ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/configs/
