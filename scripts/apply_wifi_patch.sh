65af814b128367ab489d0d50cfd243109532b646
Ryan Romig <rromig92@gmail.com>
2024-05-28 22:18:42 -0700

    fix patch script

--- scripts/apply_wifi_patch.sh
+++ scripts/apply_wifi_patch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+WIRELESS_PATCH_STRING='obj-$(CONFIG_SPARD_WLAN_SUPPORT) += uwe5622/'
 
-cp patch/Kconfig ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Kconfig
-cp patch/Makefile ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Makefile
+KCONFIG_PATCH_STRING='source "drivers/net/wireless/uwe5622/Kconfig"'
+
+
+echo -e "${WIRELESS_PATCH_STRING}" >> ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Makefile
+sed -i "/endif # WLAN/i ${KCONFIG_PATCH_STRING}" ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Kconfig
 
 # cp patch/dts/melodia.dts ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
 # cp patch/dts/Makefile ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
 
-cp patch/melodia_defconfig ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/configs/
 cp -rf ${PROJECT_ROOT_DIR}/patch/linux/* ${PROJECT_ROOT_DIR}/src/linux
