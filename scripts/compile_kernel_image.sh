5f1c6c2323c3960cfc095971b09c5a01bed06ed2
Ryan Romig <rromig92@gmail.com>
2024-05-28 21:49:51 -0700

    restructure patch

--- scripts/compile_kernel_image.sh
+++ scripts/compile_kernel_image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 (cd ${PROJECT_ROOT_DIR}/src/linux
-ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make menuconfig)
+# ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make defconfig
+# ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make menuconfig
+
 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j$(nproc) Image
 )
 cp ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/Image ${CACHE_DIR}
