f6dc55901bad694daa557ee4591ff307e2192ade
Ryan Romig <rromig92@gmail.com>
2024-05-28 22:11:48 -0700

    add additional wifi patch

--- scripts/apply_wifi_patch.sh
+++ scripts/apply_wifi_patch.sh
@@ -3,7 +3,8 @@
 cp patch/Kconfig ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Kconfig
 cp patch/Makefile ${PROJECT_ROOT_DIR}/src/linux/drivers/net/wireless/Makefile
 
-cp patch/dts/melodia.dts ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
-cp patch/dts/Makefile ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
+# cp patch/dts/melodia.dts ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
+# cp patch/dts/Makefile ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/boot/dts/allwinner/
 
 cp patch/melodia_defconfig ${PROJECT_ROOT_DIR}/src/linux/arch/arm64/configs/
+cp -rf ${PROJECT_ROOT_DIR}/patch/linux/* ${PROJECT_ROOT_DIR}/src/linux
