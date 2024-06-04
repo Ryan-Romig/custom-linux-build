54a18b9396e509e80047bbe60fef79f96aa938a0
Ryan <rromig92@gmail.com>
2024-06-04 13:11:02 -0700

    fix defconfig

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-. ./scripts/bootstrap.sh
+source scripts/bootstrap.sh
 echo $OUTPUT_FILE $OUTPUT_DIR $PROJECT_ROOT_DIR
 
 # create image file 
@@ -35,16 +35,16 @@ sudo cp $DEVICE_TREE_BINARY ${PROJECT_ROOT_DIR}/mount/boot/boot/dtb
 #sudo cp u-boot/arch/arm/dts/sun50i-h618-orangepi-zero2w.dtb mount/boot/boot/
 
 #compile boot.cmd into a boot.scr for u-boot
-mkimage -C none -A arm64 -T script -d $UBOOT_SCRIPT_FILE ${PROJECT_ROOT_DIR}/out/boot.scr
-sudo cp ${PROJECT_ROOT_DIR}/out/boot.scr ${PROJECT_ROOT_DIR}/mount/boot/boot.scr
+mkimage -C none -A arm64 -T script -d $UBOOT_SCRIPT_FILE ${UBOOT_SCRIPT_OUT_FILE}
+sudo cp ${UBOOT_SCRIPT_OUT_FILE} ${PROJECT_ROOT_DIR}/mount/boot/boot.scr
 
 #dont copy the u-boot binary like the commented out code, it needs to be at the right memory address per allwinner instructions. use dd with seek=8M
 #sudo cp u-boot/u-boot-sunxi-with-spl.bin mount/boot/
 sudo dd if=${UBOOT_FILE} of=/dev/loop88 seek=8 bs=1024 status=progress
 
 
-# curl https://images.linuxcontainers.org/images/debian/bullseye/arm64/default/20240527_05%3A24/rootfs.tar.xz -o rootfs.tar.xz >> /dev/null
-
+# curl https://images.linuxcontainers.org/images/debian/bullseye/arm64/default/20240604_05%3A24/rootfs.tar.xz -o rootfs.tar.xz >> /dev/null
+# mkdir -p ${CACHE_DIR}/rootfs
 # tar -xvf rootfs.tar.xz -C ${PROJECT_ROOT_DIR}/out/cache/rootfs/ >> /dev/null
 sudo cp -r ${PROJECT_ROOT_DIR}/out/cache/rootfs/* ${PROJECT_ROOT_DIR}/mount/rootfs >> /dev/null
 #sudo cp -r ${PROJECT_ROOT_DIR}/src/rootfs/rootfs/* ${PROJECT_ROOT_DIR}/mount/rootfs >> /dev/null
