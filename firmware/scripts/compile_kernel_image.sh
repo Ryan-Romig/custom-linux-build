984976a8dd7c153cc912be631e86d0e216500da3
Ryan <rromig92@gmail.com>
2024-07-29 06:52:36 -0700

    remove submodule linux

--- firmware/scripts/compile_kernel_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-(cd ${SUBMODULE_PATH}/linux
-ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make melodia_defconfig
-# ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make menuconfig
-
-ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j$(nproc) Image
-)
-cp ${SUBMODULE_PATH}/linux/arch/arm64/boot/Image ${CACHE_DIR}
--- firmware/scripts/toolchain.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Installing toolchain..."
-sudo apt install -y flex bison gcc-aarch64-linux-gnu swig python3-dev libssl-dev u-boot-tools qemu-user-static gcc make bc libncurses5-dev libssl-dev dosfstools python3-pip
-python3 -m pip install setuptools
