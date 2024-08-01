9b69686c673e96a9f6f6b2da9f5f65f77a5f4a1c
Ryan <rromg92@gmail.com>
2024-07-31 19:38:52 -0700

    add dts to patch for 6.6

--- firmware/scripts/toolchain.sh
+++ firmware/scripts/toolchain.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing toolchain..."
-sudo apt install -y flex bison gcc-aarch64-linux-gnu swig python3-dev libssl-dev u-boot-tools qemu-user-static gcc make bc libncurses5-dev libssl-dev dosfstools python3-pip
+sudo apt install -y parted util-linux dosfstools e2fsprogs flex bison gcc-aarch64-linux-gnu swig python3-dev libssl-dev u-boot-tools qemu-user-static gcc make bc libncurses5-dev libssl-dev dosfstools python3-pip rsync
 python3 -m pip install setuptools
